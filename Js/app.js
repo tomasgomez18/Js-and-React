5d13a5c13eb1cd57c5b8e1dd552232fa97af0f73
tomas <tomasgomezzi36@gmail.com>
2025-10-16 22:36:36 -0300

    inicie el repositorio sin ramas

---
+++ Js/app.js
@@ -0,0 +1,66 @@
+/* const contenedor = document.getElementById('contenedor');
+console.log(contenedor); 
+
+const titulo = document.getElementById('titulo');
+console.log(typeof titulo) */
+
+
+/* const toppings = document.getElementsByClassName('topping');
+console.log(toppings)
+ */
+
+/* const misToppings = document.getElementsByTagName('li');
+console.log(misToppings); */
+
+/* const aceitunas = document.querySelector('#aceitunas');
+console.log(aceitunas) */
+
+/* const toppingsNaranja = document.querySelectorAll(' .fondo-naranja ');
+console.log(toppingsNaranja); */
+
+/* const primerTopping = document.querySelector('.topping'); */
+
+//cambiando stilos con notacion de .
+/* primerTopping.style.backgroundColor = 'blue';   */  //color de fondo
+/* primerTopping.style.color='#6dff00'; */  //color de letra
+/* primerTopping.style.textTransform = 'uppercase';  */ //tamano de texto
+
+//acceder al texto 
+
+/* const listsDeToppings = document.getElementById('lista-toppings');  */
+
+//no devuelve espacios los espacios que contiene el elemento
+/* console.log('>innerText'); */
+/* console.log (listsDeToppings.innerText); */
+
+//si devuelve espacios del texto que contiene el elemento
+/* console.log('>textContent'); */
+/* console.log(listsDeToppings.textContent); */
+
+//devuelve la estructura interna del elemento html incluyendo las etiquetas
+/* console.log('>innerHTML');
+console.log(listsDeToppings.innerHTML) */
+
+//Modificar el texto del contenido 
+/* const titulo = document.getElementById('titulo');
+titulo.innerText = 'Hola Tomas' */
+
+//Modificar los atributos de un elemento 
+/* const enlace = document.getElementsByTagName('a')
+console.log(enlace [0].setAttribute('href', 'https://www.freecodecamp.org')); */
+
+//Agregar, modificar o eliminar una clase 
+/* const primerTopping = document.querySelector('.topping');     */
+/* console.log(primerTopping.classList); */ //ver la clase 
+
+//Agregar una clase y la agrege al css
+/* primerTopping.classList.add('mi-clase');
+console.log(primerTopping); */
+
+//Verificar si un elemento posee una clase 
+/* console.log(primerTopping.classList.contains('fondo-marron'));  */// devuelve true, si existe
+
+//Eliminar una clase 
+/* primerTopping.classList.remove('topping'); */
+
+
